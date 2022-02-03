10de4b027969b256e127fd9490a70a1c153e5abd
Islam <isxmadgmail.com>
2022-02-03 21:05:27 +0300

    da

--- #src/js/components/thirdPart.js
+++ #src/js/components/thirdPart.js
@@ -1,16 +1,26 @@
 export function thirdPart() {
 
-    const hiddenBlock = document.querySelector('.third__part');
-    const toggleRadioBtns = document.querySelectorAll('.hiddenBlockToggler');
+    // const hiddenBlock = document.querySelector('.third__part');
+    // const toggleRadioBtns = document.querySelectorAll('.hiddenBlockToggler');
 
-    toggleRadioBtns.forEach((btn, index) => {
-        btn.addEventListener('click', (e) => {
-            if(index == 2) {
-                hiddenBlock.style.display = 'block';
-            } else {
-                hiddenBlock.style.display = 'none'; 
-            }
-        });
-    });
+    // toggleRadioBtns.forEach((btn, index) => {
+    //     btn.addEventListener('click', (e) => {
+    //         if(index == 2) {
+    //             hiddenBlock.style.display = 'block';
+    //         }
+
+    //         if(index == 3) {
+
+    //         }
+
+    //         if(index == 1) {
+
+    //         }
+
+    //         if(index == 0) {
+
+    //         }
+    //     });
+    // });
 
 }
